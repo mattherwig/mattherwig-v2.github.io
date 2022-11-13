6005946328298bc2b9943369ad67928135ea62e9
Matt Herwig <matt.herwig@gusto.com>
2022-11-13 17:07:30 -0500

    Edit Projects

--- src/data.ts
+++ src/data.ts
@@ -152,7 +152,7 @@ export const PROJECTS: Project[] = [
   },
   {
     title: 'React Connect Four',
-    description: 'Fun little connect four project.',
+    description: 'Fun little connect four project combined with Github Pages.',
     technologies: ['React', 'HTML', 'CSS', 'Javascript'],
     website: 'mattherwig.com/react-connect-four/'
   },
